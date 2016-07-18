c346d0be11504a83f707eb3fb5ac3411fbd28ab7
clasch <clasch@live.unc.edu>
2016-07-18 14:04:05 -0400

    fixed errors in script

--- answers.sql
+++ answers.sql
@@ -1,1 +1,56 @@
-clascher,no,yes,no,2,1,2016-07-13 12:00:37,scarrasco27541,yes,no,no,8,2,2016-07-13 12:03:51,kmoney9,yes,yes,yes,1,3,2016-07-13 12:04:47,johnrmullan,Yes,No,No,3,2,2016-07-13 12:05:38,jdmar3,yes,no,no,0,2,2016-07-15 12:35:27,bob,yes,yes,no,3,2,2016-07-15 12:37:09,bill,yes,yes,yes,4,3,2016-07-15 12:37:47,unc,yes,yes,yes,1,13,2016-07-15 12:38:42,johnrmullan,yes,no,no,3,2,2016-07-15 12:41:41,clascher,no,yes,no,32,2,2016-07-15 12:44:06
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: questionnaire
+-- ------------------------------------------------------
+-- Server version	5.5.49-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `responses`
+--
+
+DROP TABLE IF EXISTS `responses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `responses` (
+  `Username` varchar(255) DEFAULT NULL,
+  `Car` varchar(255) DEFAULT NULL,
+  `OnCampus` varchar(255) DEFAULT NULL,
+  `Scholarship` varchar(255) DEFAULT NULL,
+  `Pets` int(11) DEFAULT NULL,
+  `Siblings` int(11) DEFAULT NULL,
+  `TimeStamp` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`TimeStamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `responses`
+--
+
+LOCK TABLES `responses` WRITE;
+/*!40000 ALTER TABLE `responses` DISABLE KEYS */;
+INSERT INTO `responses` VALUES ('clascher','no','yes','no',2,1,'2016-07-18 12:40:14'),('unc','no','yes','yes',1,17,'2016-07-18 12:41:41'),('clascher','no','yes','no',2,1,'2016-07-18 12:51:14'),('clascher','yes','yes','no',5,3,'2016-07-18 13:56:48');
+/*!40000 ALTER TABLE `responses` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-18 13:56:58
--- questionnaire.sh
+++ questionnaire.sh
@@ -3,6 +3,9 @@
 #This is  a questionnaire that allows for user input of answers. Then we will turn the answers into a .csv file. 
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
+echo "What is your MySQL password?"
+read PASSWORD
+
 echo "What is your Github Username?"
 read ans
 echo $ans > answers.list
@@ -29,12 +32,14 @@ echo $ans5 >> answers.list
 
 echo $TIMESTAMP >> answers.list
 
-paste -d, -s answers.list >> answers.csv
+paste -d, -s answers.list >> temp.csv
 
-paste -d, -s answers.csv > answers.sql
+mysql -u root --password=$PASSWORD questionnaire < answers.sql
 
-mysql -h LOCALHOST -u root -p "questionnaire" < answers.sql -e "LOAD DATA INFILE '/home/cabox/workspace/task-5-database-interface/answers.csv' INTO TABLE responses FIELDS TERMINATED BY ','"
+mysql -u root --password=$PASSWORD questionnaire -e "LOAD DATA INFILE '/home/cabox/workspace/task-5-database-interface/temp.csv' INTO TABLE responses FIELDS TERMINATED BY ','"
  
+mysqldump -u root --password=$PASSWORD questionnaire > answers.sql
 
+rm temp.csv
 
 
